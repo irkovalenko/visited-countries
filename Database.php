eeda8f4589df895abeb2682f23c8f8ee4af4fdb1
Irina Kovalenko <105125582+irkovalenko@users.noreply.github.com>
2026-05-04 13:45:58 +0200

    database as datasource and refactoring (#2)

---
+++ Database.php
@@ -0,0 +1,56 @@
+<?php
+
+class Database
+{
+    public $connection;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=127.0.0.1;port=3306;dbname=visited-countries;charset=utf8mb4";
+        $username = "root";
+        $password = "root";
+        try {
+            $this->connection = new PDO($dsn, $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Exception $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function query(string $query, array $params = []): \PDOStatement
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute($params);
+        return $statement;
+    }
+
+    public function countries(): array
+    {
+        return $this->query("SELECT * FROM countries")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function regions(): array
+    {
+        return $this->query("SELECT * FROM regions")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function cities(): array
+    {
+        return $this->query("SELECT * FROM cities")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getCountryByName(string $name): array|false
+    {
+        return $this->query("SELECT * FROM countries WHERE name = ?", [$name])->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getVisitedCitiesByCountry(int $countryId): array
+    {
+        return $this->query("
+            SELECT cities.name
+            FROM cities
+            INNER JOIN countries_cities ON cities.id = countries_cities.city_id
+            WHERE countries_cities.country_id = ?
+        ", [$countryId])->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
--- pages/countries_list.php
+++ pages/countries_list.php
@@ -1,15 +1,20 @@
 <?php
 
 include_once "../components/header.php";
-include_once "../data.php";
+include_once "../Database.php";
+include_once "../functions.php";
 
+$region = $_GET["cat"] ?? null;
+$db = new Database();
 
-$category = $_GET["cat"] ?? null;
+$countries = $db->countries();
+$regions = $db->regions();
+$countriesWithRegions = regionAssigner($countries, $regions);
 
-foreach ($countries as $country) {
-    if ($category && $country["category"] === $category) {
- echo "<li><h3><a href='/pages/visited_cities.php?country=" . $country["name"] ."'>" . $country["name"] . "</a></h3></li>";
-}
+foreach ($countriesWithRegions as $country) {
+    if ($region && $country["region_id"] === $region) {
+        echo "<li><h3><a href='/pages/visited_cities.php?country=" . $country["name"] . "'>" . $country["name"] . "</a></h3></li>";
+    }
 }
 
 ?>
